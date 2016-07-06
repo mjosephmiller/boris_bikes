620c21e17c21a0d3f727f9800bd819febb7f31d8
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-06 17:35:27 +0100

    Refactoring docking_station.rb, completed step 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,22 @@ class DockingStation
   end
 
   def release_bike
-    raise "Error, no bikes available." if @bikes.empty?
+    raise "Error, no bikes available." if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "Error, docking station at full capacity." if @bikes.count >= 20
+    raise "Error, docking station at full capacity." if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
