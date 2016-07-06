99bd95eb653f38ec70584bd5c44c43ef243bfa27
Jack Oddy <jackoddy91@gmail.com>
2016-07-06 11:26:02 +0100

    adds ivar to DockingStation class to store bike status

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,10 +6,10 @@ class DockingStation
   end
 
   def dock(bike)
-    bike
+    @bike = bike
   end
 
   def bike
-
+    @bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,6 +13,7 @@ describe DockingStation do
   it { is_expected.to respond_to(:bike) }
 
   it 'docks something' do
+    bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
