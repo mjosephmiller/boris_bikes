6693b620b4adf3502190076e67aba554815b9dfd
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-06 10:58:22 +0100

    Updating docking_stattion and docking_station_spec.rb step 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
+#attr_reader :number_bikes
+#  def initialize(number_bikes)
+#    @number_bikes = number_bikes
+#  end
   def release_bike
     Bike.new
+  end
+  def dock(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,7 @@
 require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock_bike) }
   it 'releases working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
