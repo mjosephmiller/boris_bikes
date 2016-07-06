0afc9bc0362e58c4640836d79141033b47005ccf
Jack Oddy <jackoddy91@gmail.com>
2016-07-06 11:44:36 +0100

    adds failure catching to release_bikes and attr_reader propertities to DockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,16 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+    describe '#release_bike' do
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+      it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+    end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
