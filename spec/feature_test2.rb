1a61718113142efb9d323da85b8fe3744cde839d
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-07 16:27:17 +0100

    refactored feature_test2.rb

--- spec/feature_test2.rb
+++ spec/feature_test2.rb
@@ -1,8 +1,10 @@
 require './lib/docking_station.rb'
 
+p "Releasing a bike that is not broken"
 station = DockingStation.new
 10.times{station.dock(Bike.new)}
 station.report_broken
-station.get_bikes
+p "We have these bikes in our station:"
+p station.get_bikes
 bmx = station.release_bike
 p "Our released bike is #{bmx}"
